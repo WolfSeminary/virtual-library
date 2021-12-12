fccaec0a0b18e5bcca04c450aed207aa0079eef9
wolf-rc <githubwolf1@gmail.com>
2021-12-12 12:08:33 +0200

    add !

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Deploy-work!
+          Edit <code>src/App.js</code> and save to reload. Deploy-work!!
         </p>
       </header>
     </div>
