1a38a649737d9bd354570b928b9e125c476ffc22
danzigerSara <sf0527696527@gmail.com>
2021-12-20 22:09:40 +0200

    add!

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <ul><li>dd</li></ul>sss
+  
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        klkl
+     
           Edit <code>src/App.js</code> and save to reload. Deploy-work!!
         </p>
       </header>
