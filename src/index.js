b613e402bcdd807ba2dae4c796ba797614cbb8be
RivkaWal <rw0533111729@gmail.com>
2021-12-26 18:08:45 +0200

    Render all books

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import books from './Books' 
 ReactDOM.render(
   <React.StrictMode>
     <App />
