7aa9caff1fc1f6a56bd3c3c16acd69c8d82022a4
Leah3716 <leahe93716@gmail.com>
2021-12-27 16:04:57 +0200

    add an AppBar

---
+++ src/AppBar.js
@@ -0,0 +1,20 @@
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+
+
+export default function LibraryAppBar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <p>Library</p>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
