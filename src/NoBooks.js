567a419c9f994cfd1f5c6823c42a92939341c8a5
Esti-Vana <esterv9413@gmail.com>
2022-01-12 14:20:09 +0200

    NoBooksComponent

---
+++ src/NoBooks.js
@@ -0,0 +1,7 @@
+import Typography from '@mui/material/Typography';
+export default  function NoBooks(){
+return (
+    <Typography  variant="body1" style={{color:"grey", margin:150+'px',fontSize:2+'rem',textAlign:"center"}}>
+        Sorry, no available books :(
+    </Typography>   
+)}
