a95fa1648a1a70f3861d466aea5792f94621bec2
Esti-Vana <esterv9413@gmail.com>
2022-01-16 18:39:32 +0200

    second-commit

--- src/NoBooks.js
+++ src/NoBooks.js
@@ -1,4 +1,6 @@
 import Typography from '@mui/material/Typography';
+import React from 'react';
+
 export default  function NoBooks(){
 return (
     <Typography  variant="body1" style={{color:"grey", margin:150+'px',fontSize:2+'rem',textAlign:"center"}}>
