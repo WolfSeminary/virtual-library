096d6080fe316aef35fda20e26fd1e2ba2314962
yael96334 <yael96334@gmail.com>
2022-01-18 20:33:51 +0200

    mediaCard

--- Book.js
+++ Book.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-
-export default function MediaCard() {
+export default function Book() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -22,8 +20,6 @@ export default function MediaCard() {
           {''}
         </Typography>
       </CardContent>
-      <CardActions>
-      </CardActions>
     </Card>
   );
 }
