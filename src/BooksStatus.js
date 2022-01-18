590af7a6b4791202f8647e3630fac52bf7762cf7
efratamar <efratamargit@gmail.com>
2022-01-18 18:58:09 +0200

    add a button

---
+++ src/BooksStatus.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function BookStatus() {
+    const getBooksStatus = () => {
+    }
+    return (
+        <>
+        <button onClick={getBooksStatus}>clickMe</button>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BooksStatus from './BooksStatus';
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  // </React.StrictMode>,
+  <BooksStatus/>,
   document.getElementById('root')
 );
 
