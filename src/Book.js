ed53d93bd41ba5654ed709bd0861b65d3e1e6cf4
yael96334 <yael96334@gmail.com>
2022-01-23 12:38:20 +0200

    update from server and change book name

--- src/Book.js
+++ src/Book.js
@@ -1,19 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function MediaCard() {
+export default function Book() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
         image={''}
-        alt="green iguana"
+        alt="BookImage"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
@@ -23,9 +21,6 @@ export default function MediaCard() {
           {''}
         </Typography>
       </CardContent>
-      <CardActions>
-       
-      </CardActions>
     </Card>
   );
 }
