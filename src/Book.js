7d68e801a22e7d3a62fa0ed9af90061ca06e9514
yael96334 <96549663+yael96334@users.noreply.github.com>
2022-01-23 12:43:20 +0200

    feat(Book): Add a cardMedia (#41)

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-export default function MediaCard() {
+export default function Book() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -22,8 +21,6 @@ export default function MediaCard() {
           {''}
         </Typography>
       </CardContent>
-      <CardActions>
-      </CardActions>
     </Card>
   );
 }
