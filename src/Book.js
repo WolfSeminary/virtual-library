d90b9f4b71ccfbc0b35b0d9f283d66feb9d916c0
yael96334 <yael96334@gmail.com>
2022-01-23 23:10:52 +0200

    update status library

--- src/Book.js
+++ src/Book.js
@@ -4,8 +4,13 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { FormControlLabel, FormGroup, Switch } from '@mui/material';
 
 export default function MediaCard() {
+  const [isFree, setIsFree] = React.useState(true)
+  const handleChange = (event) => {
+    setIsFree(event.target.checked);
+  }
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -21,9 +26,11 @@ export default function MediaCard() {
         <Typography variant="body2" color="text.secondary">
           {''}
         </Typography>
+        <FormGroup>
+          <FormControlLabel control={<Switch checked={isFree}
+            onChange={handleChange} />} label={isFree ? "free" : "borrowed"} />
+        </FormGroup>
       </CardContent>
-      <CardActions>
-      </CardActions>
     </Card>
   );
 }
--- src/BooksStatus.js
+++ src/BooksStatus.js
@@ -1,19 +1,10 @@
 import React, { useState } from 'react';
-import { Button, FormControlLabel, FormGroup } from '@mui/material';
-import { Switch } from '@mui/material';
+import { Button } from '@mui/material';
 
 export default () => {
-    const [isFree, setIsFree] = React.useState("free")
-    const handleChange = (event) => {
-        setIsFree(event.target.checked);
-    }
     return (
         <>
             <Button style={{ float: 'right' }} variant="outlined">Library Status</Button>
-            <FormGroup>
-                <FormControlLabel control={<Switch checked={isFree}
-                    onChange={handleChange} />} label={isFree ? "free" : "borrowed"} />
-            </FormGroup>
         </>
     )
 }
