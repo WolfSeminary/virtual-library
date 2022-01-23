ecfa96faea86cf98d4f10364d0e1ae45bdf38b50
RivkaWal <96003694+RivkaWal@users.noreply.github.com>
2022-01-23 15:33:36 +0200

    feat(books):Add fetch books (#43)
    * Render all books
    
    * add fetchBook component
    
    * fix branch

---
+++ src/FetchBooks.js
@@ -0,0 +1,16 @@
+import { Button, TextField } from '@mui/material'
+import { display } from '@mui/system'
+import React from 'react'
+
+function FetchBooks() {
+    return (
+        <div style={{ display: "flex", direction: "rtl" }}>
+            <TextField id="outlined-basic" label="Fetch Books" variant="outlined" />
+            <br></br>
+            <Button variant="outlined">Fetch Books</Button>
+        </div>
+    )
+}
+
+export default FetchBooks
+
