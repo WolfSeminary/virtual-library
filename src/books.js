2d32cd9d493d9e2d89be411f501c9df2057bc5af
Esti-Vana <esterv9413@gmail.com>
2022-01-23 18:28:35 +0200

    first commit

--- src/books.js
+++ src/books.js
@@ -1,15 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { useEffect } from "react"
 
-export function Books() {
+export function Books(onTopicChange) {  
+  const [differentTopic, setDifferentTopic] = useState();
+  function onTopicChange(value) {
+    setDifferentTopic(value);
+  }
   useEffect(() => {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40`).then(response => response.json()).then(
       data => {
         console.log(data)
-      }
-    )
+      })
   }, [])
   return (
-    <></>
-  )
+    <>
+      <input type="text" onChange={(e) => onTopicChange(e.target.value)} />
+    </>
+  );
 }
