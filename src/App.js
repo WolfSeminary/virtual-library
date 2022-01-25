f8e7f8376fbf4292c24e8b7a52a10af0f5c3bbcf
Leah3716 <hitmachut@students.local>
2022-01-25 18:29:12 +0200

    addnavigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import {Route,Routes} from "react-router-dom"
 import './App.css';
 function App() {
   return (
-<></>
+<>
+<Routes>
+  <Route path="/bookInfo/:id" element={<BookInfo/>}/>
+</Routes>
+</>
+
   );
 }
 
--- src/Books.js
+++ src/Books.js
@@ -3,6 +3,8 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { useNavigate } from 'react-router-dom';
+
 //( book) קבלתי קוד שמתבסס על ביצוע של בנות אחרות
 //עשיתי כמיטב יכולתי מחכה להמשך הוראות
 const Item = styled(Paper)(({ theme }) => ({
@@ -13,15 +15,20 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function NestedGrid() {
+  let navigate = useNavigate();
+  function onBookClick(id) {
+    navigate(`/bookInfo/${id}`);
+  }
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={1}>
-          {books.map(book=>
-            <Grid item xs={4}>
-            <Item>Item</Item>
+        {books.map(book =>
+          <Grid item xs={4}>
+            <div onClick={onBookClick(book.id)}>{book}</div>
           </Grid>
-         )}      
+        )}
       </Grid>
     </Box>
+
   );
 }
