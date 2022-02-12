5276cbc2c7e99c5fee3615c9e5d88887cc74c142
Esti-Vana <esterv9413@gmail.com>
2022-02-12 21:14:18 +0200

    second

--- src/books.js
+++ src/books.js
@@ -1,20 +1,54 @@
-import React, { useState } from "react"
-import { useEffect } from "react"
+import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import BooksStatusModal from "./BooksStatusModal"
+import { useNavigate } from 'react-router-dom';
+import Book from './Book';
 
-export function Books(onTopicChange) {  
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Books() {
+  let navigate = useNavigate();
+  function onBookClick(id) {
+    navigate(`/bookInfo/${id}`);
+  }
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+    fetch("https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40")
+      .then((res) => res.json())
+      .then((res) => {
+        setBooks(res.items.map(book => ({ ...book, status: "free" })))
+      })
+  }, [])
+
+  const filterBooks = React.useCallback(() => {
+    return books.filter(book => Object.keys(book).find(key => book[key].includes(searchTerm)))
+  }, [searchTerm, books])
+
+  const filteredBooks = filterBooks();
   const [differentTopic, setDifferentTopic] = useState();
   function onTopicChange(value) {
     setDifferentTopic(value);
   }
-  useEffect(() => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=Android&&maxResults=40`).then(response => response.json()).then(
-      data => {
-        console.log(data)
-      })
-  }, [])
+
   return (
-    <>
-      <input type="text" onChange={(e) => onTopicChange(e.target.value)} />
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        {books.map(book =>
+          <Grid item xs={4} key={book.id}>
+            <div onClick={() => onBookClick(book.id)}><Book book={book} /></div>
+            <BooksStatusModal />
+          </Grid>
+        )}
+      </Grid>
+      <input typef="text" onChange={(e) => onTopicChange(e.target.value)} />
+    </Box>
   );
 }
