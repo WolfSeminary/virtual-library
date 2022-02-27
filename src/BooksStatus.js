074b5ad05906c20bbdfd59d2fe982238c73d4bd5
yael96334 <yael96334@gmail.com>
2022-02-27 22:26:41 +0200

    TriggerEvent

---
+++ src/BooksStatus.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+export const BookStatus = () => {
+    const getBooksStatus=()=>{
+    }
+    return (
+        <Button onClick={getBooksStatus}>getBooksStatus</Button>
+    )
+}
